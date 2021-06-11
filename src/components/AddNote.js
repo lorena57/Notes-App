5e81aeb32e89231629a945a73a83a8d97649c4cc
Lorena C <lorena@eunknown.org>
2021-06-11 14:38:29 -0700

    add eventhandler to create new note

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const AddNote = () => {
+const AddNote = ({ handleAddNote }) => {
+  const [noteText, setNoteText] = useState('');
+
+  const handleChange = (event) => {
+    setNoteText(event.target.value);
+  };
+
+  const handleSaveClick = () => {
+    handleAddNote(noteText);
+  };
+
   return (
     <div className="note new">
       <textarea
         cols="8"
         rows="10"
         placeholder="Type to add a note..."
+        value={noteText}
+        onChange={handleChange}
       ></textarea>
       <div className="note-footer">
         <small>200 Remaining</small>
-        <button className="save">Save</button>
+        <button className="save" onClick={handleSaveClick}>
+          Save
+        </button>
       </div>
     </div>
   );
